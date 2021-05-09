b17bef499594ff753814785214d6269335e64e7e
Aveek Saha <aveek.s98@gmail.com>
2021-05-09 20:27:08 +0530

    Show rendered markdown

--- api/index.js
+++ api/index.js
@@ -1,9 +1,14 @@
-module.exports = (req, res) => {
-    const { id = 'World' } = req.query
-
+module.exports = async (req, res) => {
+    const { id = '' } = req.query
+    const { Octokit } = require("@octokit/rest");
     var MarkdownIt = require('markdown-it'),
-    md = new MarkdownIt();
-    var result = md.render('# markdown-it rulezz!');
+    
+    const octokit = new Octokit();
+    const md = new MarkdownIt();
+
+    var gist = await octokit.gists.get({ gist_id: id });
+    var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
+    var result = md.render(markdown);
 
     res.send(result)
 }
