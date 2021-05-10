6743250ff5b7ecee7477694b8549313a7fac37e6
Aveek Saha <aveek.s98@gmail.com>
2021-05-10 23:09:51 +0530

    Add nunjucks for templating

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 module.exports = async (req, res) => {
     const { id = '' } = req.query
     const { Octokit } = require("@octokit/rest");
-    var MarkdownIt = require('markdown-it'),
+    var MarkdownIt = require('markdown-it');
+    var nunjucks = require('nunjucks');
     
     const octokit = new Octokit();
     const md = new MarkdownIt();
+    nunjucks.configure( 'templates', {
+        autoescape: true
+    } ) ;
 
     var gist = await octokit.gists.get({ gist_id: id });
     var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
     var result = md.render(markdown);
 
-    res.send(result)
+    var html = nunjucks.render('index.njk', { username: 'James' });
+
+    res.send(html)
 }
