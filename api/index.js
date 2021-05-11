ce024140bbb967b3accc744dcc43398e1805cf33
Aveek Saha <aveek.s98@gmail.com>
2021-05-11 11:19:49 +0530

    Added more templates and styles

--- api/index.js
+++ api/index.js
@@ -3,18 +3,32 @@ module.exports = async (req, res) => {
     const { Octokit } = require("@octokit/rest");
     var MarkdownIt = require('markdown-it');
     var nunjucks = require('nunjucks');
+    var hljs = require('highlight.js');
     
     const octokit = new Octokit();
-    const md = new MarkdownIt();
+    const md = new MarkdownIt({
+        html: true,
+        linkify: true,
+        typographer: true,
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+              try {
+                return hljs.highlight(str, { language: lang }).value;
+              } catch (__) {}
+            }
+        
+            return ''; // use external default escaping
+          }
+      });
     nunjucks.configure( 'templates', {
-        autoescape: true
+        autoescape: false
     } ) ;
 
     var gist = await octokit.gists.get({ gist_id: id });
     var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
     var result = md.render(markdown);
 
-    var html = nunjucks.render('index.njk', { username: 'James' });
+    var html = nunjucks.render('layout.njk', { username: 'James', content: result });
 
     res.send(html)
 }
