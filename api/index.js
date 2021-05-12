ef2a96a85b7aa579b815a04cc3a10154f8f662ed
Aveek Saha <aveek.s98@gmail.com>
2021-05-12 20:34:20 +0530

    Add avatar and links in header

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ module.exports = async (req, res) => {
     var nunjucks = require('nunjucks');
     var hljs = require('highlight.js');
     
-    const template = join(__dirname, 'templates')
-    console.log(template);
+    const template = join(__dirname, 'templates');
+
     const octokit = new Octokit();
     const md = new MarkdownIt({
         html: true,
@@ -31,7 +31,12 @@ module.exports = async (req, res) => {
     var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
     var result = md.render(markdown);
 
-    var html = nunjucks.render('layout.njk', { username: 'James', content: result });
+    var html = nunjucks.render('layout.njk', { 
+        owner: gist.data.owner, 
+        url: gist.data.html_url,
+        id: gist.data.id,
+        content: result 
+    });
 
     res.send(html)
 }
