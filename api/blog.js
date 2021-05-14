8efd7013eee880f4afc6ebbd3fd7b33eca8d4747
Aveek Saha <aveek.s98@gmail.com>
2021-05-14 20:19:57 +0530

    Filter gists for blog

--- api/blog.js
+++ api/blog.js
@@ -8,5 +8,15 @@ module.exports = async (req, res) => {
 
     const octokit = new Octokit();
 
-    res.send(username)
+    var gists = await octokit.gists.listForUser({
+      username
+    });
+
+    var mdFiles = gists.data.filter(function (e) {
+      var file = e.files[Object.keys(e.files)[0]]
+      var filename = file.filename.split('.')
+      return file.language == "Markdown" && filename[filename.length - 2].endsWith("_post");
+    });
+
+    res.send(mdFiles)
 }
