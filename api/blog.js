63c6a6bc9691b955358f098ab7787e42cd7aba8a
Aveek Saha <aveek.s98@gmail.com>
2021-05-14 10:52:18 +0530

    Format code

--- api/blog.js
+++ api/blog.js
@@ -1,89 +1,91 @@
 module.exports = async (req, res) => {
-    const { username = '', page = 1 } = req.query
+    const { username = "", page = 1 } = req.query;
 
     const { Octokit } = require("@octokit/rest");
-    const { join } = require('path');
-    const { load } = require('js-yaml');
-    const nunjucks = require('nunjucks');
-    
-    const template = join(__dirname, 'templates');
-    nunjucks.configure( template, {
+    const { join } = require("path");
+    const { load } = require("js-yaml");
+    const nunjucks = require("nunjucks");
+
+    const template = join(__dirname, "templates");
+    nunjucks.configure(template, {
         autoescape: false
     });
 
     const octokit = new Octokit();
-    var ids = []
-    var meta = []
-    var owner
+    var ids = [];
+    var meta = [];
+    var owner;
 
     var gists = await octokit.gists.listForUser({
-      username
+        username
     });
 
     var mdFiles = gists.data.filter(function (e) {
-      var file = e.files[Object.keys(e.files)[0]]
-      var filename = file.filename.split('.')
-      return file.language == "Markdown" && filename[filename.length - 2].endsWith("_post");
+        var file = e.files[Object.keys(e.files)[0]];
+        var filename = file.filename.split(".");
+        return (
+            file.language == "Markdown" &&
+            filename[filename.length - 2].endsWith("_post")
+        );
     });
 
     mdFiles.sort((a, b) => {
-      var dateA = new Date(a.created_at)
-      var dateB = new Date(b.created_at)
-      return dateA > dateB
+        var dateA = new Date(a.created_at);
+        var dateB = new Date(b.created_at);
+        return dateA > dateB;
     });
 
-
-    mdFiles.forEach(file => {
-      ids.push(file.id)
+    mdFiles.forEach((file) => {
+        ids.push(file.id);
     });
 
-    var page_ids = ids.slice((page-1)*5, page*5)
-    var num_pages = Math.ceil(ids.length/5)
+    var page_ids = ids.slice((page - 1) * 5, page * 5);
+    var num_pages = Math.ceil(ids.length / 5);
 
     const findMetadataIndices = (mem, item, i) => {
-      if (/^---/.test(item)) {
-        mem.push(i)
-      }
-      return mem
-    }
+        if (/^---/.test(item)) {
+            mem.push(i);
+        }
+        return mem;
+    };
 
     const parseMetadata = ({ lines, metadataIndices }) => {
-      if (metadataIndices.length > 0) {
-        let metadata = lines.slice(metadataIndices[0] + 1, metadataIndices[1])
-        return load(metadata.join('\n'))
-      }
-      return {}
-    }
+        if (metadataIndices.length > 0) {
+            let metadata = lines.slice(
+                metadataIndices[0] + 1,
+                metadataIndices[1]
+            );
+            return load(metadata.join("\n"));
+        }
+        return {};
+    };
 
-    const parseMD = contents => {
-      const lines = contents.split('\n')
-      const metadataIndices = lines.reduce(findMetadataIndices, [])
-      const metadata = parseMetadata({ lines, metadataIndices })
-      return metadata
-    }
-
+    const parseMD = (contents) => {
+        const lines = contents.split("\n");
+        const metadataIndices = lines.reduce(findMetadataIndices, []);
+        const metadata = parseMetadata({ lines, metadataIndices });
+        return metadata;
+    };
 
     for (const id of page_ids) {
-      var gist = await octokit.gists.get({ gist_id: id });
-      var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
-      var parsed = parseMD(markdown)
-      var data = {
-        title: parsed.title,
-        description: parsed.description,
-        id: id
-      }
-      meta.push(data)
+        var gist = await octokit.gists.get({ gist_id: id });
+        var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content;
+        var parsed = parseMD(markdown);
+        var data = {
+            title: parsed.title,
+            description: parsed.description,
+            id: id
+        };
+        meta.push(data);
     }
 
+    if (mdFiles.length > 0) owner = mdFiles[0].owner;
 
-    if (mdFiles.length > 0)
-      owner = mdFiles[0].owner
-
-    var html = nunjucks.render('blog.html', { 
-      data: meta,
-      owner: owner,
-      num_pages: num_pages
+    var html = nunjucks.render("blog.html", {
+        data: meta,
+        owner: owner,
+        num_pages: num_pages
     });
 
-    res.send(html)
-}
+    res.send(html);
+};
--- api/post.js
+++ api/post.js
@@ -1,13 +1,13 @@
 module.exports = async (req, res) => {
-    const { id = '' } = req.query
+    const { id = "" } = req.query;
     const { Octokit } = require("@octokit/rest");
-    const { join } = require('path');
-    const MarkdownIt = require('markdown-it');
-    const nunjucks = require('nunjucks');
-    const hljs = require('highlight.js');
-    const meta = require('markdown-it-meta');
-    
-    const template = join(__dirname, 'templates');
+    const { join } = require("path");
+    const MarkdownIt = require("markdown-it");
+    const nunjucks = require("nunjucks");
+    const hljs = require("highlight.js");
+    const meta = require("markdown-it-meta");
+
+    const template = join(__dirname, "templates");
 
     const octokit = new Octokit();
     const md = new MarkdownIt({
@@ -16,39 +16,36 @@ module.exports = async (req, res) => {
         typographer: true,
         highlight: function (str, lang) {
             if (lang && hljs.getLanguage(lang)) {
-              try {
-                return hljs.highlight(str, { language: lang }).value;
-              } catch (__) {}
+                try {
+                    return hljs.highlight(str, { language: lang }).value;
+                } catch (__) {}
             }
-        
-            return ''; // use external default escaping
-          }
-      });
-    md.use(meta)
-    nunjucks.configure( template, {
+
+            return ""; // use external default escaping
+        }
+    });
+    md.use(meta);
+    nunjucks.configure(template, {
         autoescape: false
-    } ) ;
+    });
 
     var gist = await octokit.gists.get({ gist_id: id });
-    var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content
+    var markdown = gist.data.files[Object.keys(gist.data.files)[0]].content;
     var result = md.render(markdown);
 
-    var description = "A Gist Blog post"
-    var title = "Gist Blog"
-    if(md.meta.description)
-        description = md.meta.description
-    if(md.meta.title)
-        title = md.meta.title
-    
+    var description = "A Gist Blog post";
+    var title = "Gist Blog";
+    if (md.meta.description) description = md.meta.description;
+    if (md.meta.title) title = md.meta.title;
 
-    var html = nunjucks.render('post.html', { 
-        owner: gist.data.owner, 
+    var html = nunjucks.render("post.html", {
+        owner: gist.data.owner,
         url: gist.data.html_url,
         id: gist.data.id,
         description: description,
         title: title,
-        content: result 
+        content: result
     });
 
-    res.send(html)
-}
+    res.send(html);
+};
